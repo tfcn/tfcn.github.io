b90877e119531ef39c7ac389ca202bfcedf5051c
tfcn <tfcn@users.noreply.github.com>
2015-04-27 01:31:37 +0800

    Fix missing word around break lines

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   $('.post-content > p').each(function () {
     $this = $(this);
     $this.text($this.text().replace(
-          /[\u4E00-\u9FA5\uF900-\uFA2D]\n[\u4E00-\u9FA5\uF900-\uFA2D]/, ''));
+          /([\u4E00-\u9FA5\uF900-\uFA2D])\n([\u4E00-\u9FA5\uF900-\uFA2D])/, '$1$2'));
   });
 
   // Warning dialog.
