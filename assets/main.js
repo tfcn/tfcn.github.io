5773b44bdc6fbae0bea04502ac6580c95cd157e9
tfcn <tfcn@users.noreply.github.com>
2015-04-26 23:59:03 +0800

    Adult content warning

--- assets/main.js
+++ assets/main.js
@@ -6,4 +6,19 @@ $(document).ready(function () {
     $this.text($this.text().replace(
           /[\u4E00-\u9FA5\uF900-\uFA2D]\n[\u4E00-\u9FA5\uF900-\uFA2D]/, ''));
   });
+
+  // Warning dialog.
+  $warning = $('#adult-warning');
+  $warning.find('.enter').click(function () {
+    $warning.fadeOut();
+    Cookies.set('show-adult', true, { expires: 7, path: '/story/adult/' });
+  });
+  $warning.find('.leave').click(function () {
+    history.back();
+  });
+  if (Cookies.get('show-adult') == undefined) {
+    $warning.css('display', 'block');
+  }
+
+
 });
