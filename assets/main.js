b0924649dfd8e54ed34dae719127abd527477bee
tfcn <tfcn@users.noreply.github.com>
2015-04-27 15:39:40 +0800

    Fix HTML destroy

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   // Remove line break.
   $('.post-content > p').each(function () {
     $this = $(this);
-    $this.text($this.text().replace(
+    $this.html($this.html().replace(
           /([\u4E00-\u9FA5\uF900-\uFA2D])\n([\u4E00-\u9FA5\uF900-\uFA2D])/, '$1$2'));
   });
 
